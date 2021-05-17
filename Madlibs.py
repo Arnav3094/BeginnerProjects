1097ba6906b293f7cb79a0a13188409a0dfc439b
Arnav3094 <Marnav2004>
2021-05-17 19:14:40 +0530

    add code to Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+# Inputs
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"\nComputer Programming is so {adj}!\nIt makes me so excited because I love to {verb1}.\nIt makes me want to {verb2} like {famous_person}\n"
+    
+print(madlib)
